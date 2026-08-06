929ec16add6d3e26db4306228dbef463e2a7f773
ZeroMeOut <wiziset@gmail.com>
2026-08-06 23:16:58 +0100

    speed to acceleration naming change, made a text object

--- game_core/game_objects.py
+++ game_core/game_objects.py
@@ -2,24 +2,24 @@ import pygame
 
 ## This is from https://github.com/phildecroos/lunar_lander/blob/master/lunar_lander.py
 class GameObject(pygame.sprite.Sprite):
-    def __init__(self, x: float, y: float, x_speed: float, y_speed: float, name: str):
+    def __init__(self, x: float, y: float, x_acceleration: float, y_acceleration: float, name: str):
         self.image = pygame.image.load(f"assets/{name}.png")
         self.store_x = x
         self.store_y = y
-        self.store_x_speed = x_speed
-        self.store_y_speed = y_speed
+        self.store_x_acceleration = x_acceleration
+        self.store_y_acceleration = y_acceleration
         self.x = x
         self.y = y
-        self.x_speed = x_speed
-        self.y_speed = y_speed
+        self.x_acceleration = x_acceleration
+        self.y_acceleration = y_acceleration
         self.name = name
         self.rect = self.image.get_rect()
         self.rect.topleft = (int(self.x), int(self.y))
 
     # Change the x and y values of the object
     def move(self):
-        self.x += self.x_speed
-        self.y += self.y_speed
+        self.x += self.x_acceleration
+        self.y += self.y_acceleration
         self.rect.topleft = (int(self.x), int(self.y))
 
     # Move the object's rect and display its image
@@ -32,36 +32,41 @@ class GameObject(pygame.sprite.Sprite):
     def reset(self):
         self.x = self.store_x
         self.y = self.store_y
-        self.x_speed = self.store_x_speed
-        self.y_speed = self.store_y_speed
+        self.x_acceleration = self.store_x_acceleration
+        self.y_acceleration = self.store_y_acceleration
 
-## Button code from https://github.com/baraltech/Menu-System-PyGame
-class Button():
-	def __init__(self, image, pos, text_input, font, base_color, hovering_color):
-		self.image = image
-		self.x_pos = pos[0]
-		self.y_pos = pos[1]
-		self.font = font
-		self.base_color, self.hovering_color = base_color, hovering_color
-		self.text_input = text_input
-		self.text = self.font.render(self.text_input, True, self.base_color)
-		if self.image is None:
-			self.image = self.text
-		self.rect = self.image.get_rect(center=(self.x_pos, self.y_pos))
-		self.text_rect = self.text.get_rect(center=(self.x_pos, self.y_pos))
+## Made from https://github.com/baraltech/Menu-System-PyGame
+class TextObject:
+    def __init__(self, text_input, font, color, pos):
+        self.text_input = text_input
+        self.font = font
+        self.color = color
+        self.x_pos, self.y_pos = pos
+        self.text_surface = font.render(text_input, True, color)
+        self.rect = self.text_surface.get_rect(center=pos)
 
-	def update(self, screen):
-		if self.image is not None:
-			screen.blit(self.image, self.rect)
-		screen.blit(self.text, self.text_rect)
+    def render(self, color):
+        self.text_surface = self.font.render(self.text_input, True, color)
 
-	def checkForInput(self, position):
-		if position[0] in range(self.rect.left, self.rect.right) and position[1] in range(self.rect.top, self.rect.bottom):
-			return True
-		return False
+    def update(self, screen):
+        screen.blit(self.text_surface, self.rect)
 
-	def changeColor(self, position):
-		if position[0] in range(self.rect.left, self.rect.right) and position[1] in range(self.rect.top, self.rect.bottom):
-			self.text = self.font.render(self.text_input, True, self.hovering_color)
-		else:
-			self.text = self.font.render(self.text_input, True, self.base_color)
+
+class Button(TextObject):
+    def __init__(self, image, pos, text_input, font, base_color, hovering_color):
+        super().__init__(text_input, font, base_color, pos)
+        self.base_color, self.hovering_color = base_color, hovering_color
+        self.image = image if image is not None else self.text_surface
+        self.rect = self.image.get_rect(center=pos)
+        self.text_rect = self.text_surface.get_rect(center=pos)
+
+    def update(self, screen):
+        if self.image is not None:
+            screen.blit(self.image, self.rect)
+        screen.blit(self.text_surface, self.text_rect)
+
+    def checkForInput(self, position):
+        return self.rect.collidepoint(position)
+
+    def changeColor(self, position):
+        self.render(self.hovering_color if self.rect.collidepoint(position) else self.base_color)
